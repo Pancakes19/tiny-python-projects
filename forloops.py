da5bf5880bcfa230c99badaa088d30b94062ec83
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-17 21:48:20 +0200

    iterating over strings

--- forloops.py
+++ forloops.py
@@ -6,12 +6,16 @@
 #     print('*')
 
 # or print without a counter using range
-for x in reversed(range(1,11)):
-    print(x)
-print("Happy New year")    
+# we can use range with a step to count in steps e.g range(1, 11, 2)
+# for x in reversed(range(1,11)):
+#     print(x)
+# print("Happy New year!")    
     
-    
-    
+# we can also iterate thru a string
+myNumber = '0815649939'
+
+for x in myNumber:
+    print(x,end='')
     
     
     

a24e443a609500732bb0b157202b8ce1392128a2
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-02-17 21:43:21 +0200

    forloops

---
+++ forloops.py
@@ -0,0 +1,17 @@
+# for loops in python
+# u can iterate over a range, string, sequence etc.
+
+# counter = 10
+# for i in range(counter):
+#     print('*')
+
+# or print without a counter using range
+for x in reversed(range(1,11)):
+    print(x)
+print("Happy New year")    
+    
+    
+    
+    
+    
+    
